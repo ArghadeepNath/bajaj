ba163d33aa30419c4bbe15ff4009e3f7ae6af5b7
Arghadeep <arghadeep28@gmail.com>
2025-07-29 20:47:04 +0530

    Hosting again

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bfhlRoute = require('../route/route');
+const serverless = require('serverless-http');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,3 +9,4 @@ app.use(bodyParser.json());
 app.use('/bfhl', bfhlRoute);
 
 module.exports = app;
+module.exports.handler = serverless(app);
