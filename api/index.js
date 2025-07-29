b29a0147819edb2e08269218cc9a03f014e02468
Arghadeep <arghadeep28@gmail.com>
2025-07-29 20:50:18 +0530

    Host

--- api/index.js
+++ api/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const serverless = require('serverless-http'); // NEW
 const bodyParser = require('body-parser');
-const bfhlRoute = require('../route/route');
-const serverless = require('serverless-http');
+const bfhlRoute = require('./route'); // adjusted path
 
 const app = express();
 app.use(bodyParser.json());
-
 app.use('/bfhl', bfhlRoute);
 
 module.exports = app;
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(app); // IMPORTANT
